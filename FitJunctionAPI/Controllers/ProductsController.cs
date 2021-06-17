3a9318ebd1691b17d14a9d6a0e1614fac03e7263
Umer Nawab <umer@intelligenes.net>
2021-06-17 14:02:07 +0500

    added product model and controller

---
+++ FitJunctionAPI/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace FitJunctionAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+      
+
+        private List<Product> Products = new List<Product> {
+
+            new Product() { ProductId = 1,  ProductName = "Nike round neck T-Shirt", Price=30, CategoryId=1},
+            new Product() { ProductId = 2,  ProductName = "Adidas running Track Suit", Price=90, CategoryId=2},
+            new Product() {ProductId = 3,  ProductName = "Speedo Swimming Suit", Price=80, CategoryId=3},
+            new Product() { ProductId = 4,  ProductName = "Dunlop Tennis Gear", Price=50, CategoryId=4}
+        };
+
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+
+            return Products;
+
+
+        }
+
+    }
+}
---
+++ FitJunctionAPI/Models/Product.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace FitJunctionAPI
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int Price { get; set; }
+
+
+
+
+    }
+}
